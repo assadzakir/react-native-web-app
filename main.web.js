eb4684688bf5b92efd17c1a1ae89e485598b8538
Assad zakir <assadzakirhussain@gmail.com>
2017-05-21 11:04:15 +0500

    setup react-expo client and react-web client

---
+++ main.web.js
@@ -0,0 +1,8 @@
+/* 
+    ./main.web.js
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/web/components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
